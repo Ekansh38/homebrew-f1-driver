0bf08d6df689348a44eecfaf8b6dbcd568caa701
Byte Colony <goenka.ekansh@gmail.com>
2026-04-13 16:29:08 +0800

    chore: bump to v1.2.0

--- Formula/f1-driver.rb
+++ Formula/f1-driver.rb
@@ -1,12 +1,12 @@
 class F1Driver < Formula
   desc "Top-down F1 racing simulator with signed lap times"
   homepage "https://github.com/Ekansh38/f1-driver"
-  version "1.0.1"
+  version "1.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/Ekansh38/f1-driver/releases/download/v1.0.1/f1-driver-macos-arm64.tar.gz"
-      sha256 "900e07788394f8fcb757690777d63a026c538f2f6890cfecc3a901ce53ca95e6"
+      url "https://github.com/Ekansh38/f1-driver/releases/download/v1.2.0/f1-driver-macos-arm64.tar.gz"
+      sha256 "11b8e0b2a7bb3cb293e3f155a7c17e01f53ffdb562dd81a0882ecdeb70a78ec9"
     end
   end
 
